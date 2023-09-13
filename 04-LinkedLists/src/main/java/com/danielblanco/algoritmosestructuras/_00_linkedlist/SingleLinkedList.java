2ebd32555435047408f804afbc3929b83b9aea51
Humberto Arellano <97417893+HumbertoArellano@users.noreply.github.com>
2023-09-12 20:56:49 -0700

    Update SingleLinkedList.java

--- 04-LinkedLists/src/main/java/com/danielblanco/algoritmosestructuras/_00_linkedlist/SingleLinkedList.java
+++ 04-LinkedLists/src/main/java/com/danielblanco/algoritmosestructuras/_00_linkedlist/SingleLinkedList.java
@@ -1,14 +1,33 @@
 package com.danielblanco.algoritmosestructuras._00_linkedlist;
 
 public class SingleLinkedList {
-  public Node head;
+  public Node head; // Nodo auxiliar
 
-  public void appendToTail(int value) {
+
+  /*     head = null;
+  
+     Agregar un elemento al final 
+     Si la cabezera en donde se encuentra el nodo es null
+     Crear un nuevo nodo con el valor que le estoy dando como parametro
+     y lo devuelves.
+  */
+  public void appendToTail(int value) { // 
     if (head == null) {
-      head = new Node(value);
-      return;
+      head = new Node(value);  
+      return;                  
     }
+    
+      /*
+        Si el nodo es diferente de null.
+        Entonces Se crea un variable de nodo auxiliar que guarde el head actual.
 
+        Relizamos una busqueda para verificar si el siguiente nodo no es null
+          Avanzamos al siguiente nodo.
+
+          Si el siguiente nodo es null, salimos del bucle y al nodo siguiente
+          le agregamos un nuevo nodo con el valor que pasamos como parametro.
+      */
+    
     Node current = head;
     while (current.next != null) {
       current = current.next;
